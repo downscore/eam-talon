30f4239001020fe214f66f29b6dcbd40a2e54d21
Eam <eam.doherty@gmail.com>
2024-05-04 20:48:00 -0700

    Add steam app with support for opening games directory.

---
+++ apps/steam.py
@@ -0,0 +1,28 @@
+"""Talon code for Steam support."""
+# Disable linter warnings caused by Talon conventions.
+# pylint: disable=no-self-argument, no-method-argument, relative-beyond-top-level
+# pyright: reportSelfClsParameterName=false, reportGeneralTypeIssues=false
+# mypy: ignore-errors
+
+from talon import Context, Module
+
+mod = Module()
+ctx = Context()
+
+mod.apps.steam = """
+os: mac
+and app.bundle: com.valvesoftware.steam.helper
+"""
+
+ctx.matches = r"""
+app: steam
+"""
+
+
+@ctx.action_class("user")
+class ExtensionActions:
+  """Action overwrites."""
+
+  def switcher_get_current_directory() -> str:
+    # Use the Anki user data directory as current.
+    return "/Users/${USER}/Library/Application Support/Steam/steamapps/common/"

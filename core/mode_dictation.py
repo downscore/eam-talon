004a65d293120596bb8245d673b5d6355f478b79
Eam <eam.doherty@gmail.com>
2024-07-16 20:15:42 -0700

    Fix spacing around @ characters in dictation mode.

--- core/mode_dictation.py
+++ core/mode_dictation.py
@@ -31,6 +31,10 @@ def _space_required(preceding_text: str, next_text: str) -> bool:
   preceding_char = preceding_text[-1]
   next_char = next_text[0]
 
+  # Special case: No space between alphanumeric text and an @ symbol.
+  if preceding_char.isalnum() and next_char == "@":
+    return False
+
   # Special case: No space after quotes if there is nothing before it.
   if len(preceding_text) == 1 and preceding_char in ("'", "\""):
     return False
